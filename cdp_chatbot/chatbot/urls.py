c144b6bd2fa5c37c3d643ad1fa56202737eaca4a
SohamSawalakhe <sohamsawalakhe@gmail.com>
2025-01-11 17:07:10 +0530

    Delete cdp_chatbot directory

--- cdp_chatbot/chatbot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Show the chatbot interface
-    path('ask/', views.ask_question, name='ask_question'),
-     path('', views.ask_question, name='ask_question'),  # Handle user questions
-]
